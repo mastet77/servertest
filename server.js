7c189a433b2759816845ac465e81d21c4ffe0cd6
Ihor <ihor.aleksejev@gmail.com>
2026-02-22 20:18:10 +0200

    formatting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello, world!\n");
+    res.end("<b><i>Hello</i>, world!\n</b>");
 }).listen(port, () => {
     console.log("Server running on port " + port);
 });
