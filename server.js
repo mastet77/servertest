519d14182ba778775d213e7bc99896d2771b4c0c
Максим Брагин <makstet007@gmail.com>
2026-02-22 19:34:42 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// http://localhost:3000
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, world!\n");
+}).listen(port, () => {
+    console.log("Server running on port " + port);
+});
