0c94471481cc49f7d396317f7c592124c190c45d
Максим Брагин <makstet007@gmail.com>
2026-02-22 21:54:54 +0300

    все обьяснения в коде

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-// http://localhost:3000
 const http = require("http");
 
 const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("<b><i>Hello</i>, world!\n</b>");
+    // говорим браузеру, что будет HTML
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+    // HTML с стилями
+    res.end(`
+    <div style="
+      font-weight: bold;       /* жирный */
+      font-size: 3em;          /* примерно в 3 раза больше обычного */
+      color: white;            /* белый цвет */
+      background-color: black; /* фон, чтобы белый был виден */
+      text-align: center;      /* по центру */
+      margin-top: 20%;
+    ">
+      Hello, world!
+    </div>
+  `);
 }).listen(port, () => {
     console.log("Server running on port " + port);
 });
