80e7ca717a6d554f462e11762b3a3bb0aa1f55cd
Максим Брагин <makstet007@gmail.com>
2026-02-25 23:55:03 +0300

    добавил API и улучшил код

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
+// cd C:\code\servertest
+// req - запрос, res - ответ
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+  res.send("Hello from Maxim!");
+});
+
+const teams = [{ "name": "Team A" }];
+
+app.post("/api/teams", (req, res) => {
+  const team = {
+    id: Date.now(),
+    name: req.body.name
+  };
+
+  teams.push(team);
+  res.json(team);
+});
+
+app.get("/api/teams/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+
+  if (!team) {
+    return res.status(404).json({ error: "Not Found" });
+  }
+
+  res.json(team);
 });
 
 app.listen(PORT, () => {
